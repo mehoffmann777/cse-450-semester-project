0578c0cf4729319c9fa540a0c6aa586394eeae5e
mehoffmann777 <mehoffmann777@users.noreply.github.com>
2024-02-09 16:34:17 -0600

    added scene transitions

--- cse450_group_project/Assets/Code/SceneTransition.cs
+++ cse450_group_project/Assets/Code/SceneTransition.cs
@@ -10,7 +10,7 @@ public class SceneTransition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bundle = AssetBundle.LoadFromFile("Assets/Scenes");
+       
     }
 
     // Update is called once per frame
