58591225a7f6f4e5c75aa26361cad25e0378918b
mehoffmann777 <mehoffmann777@users.noreply.github.com>
2024-02-09 16:01:59 -0600

    added transition between prototype scenes

---
+++ cse450_group_project/Assets/Code/SceneTransition.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+
+    private AssetBundle bundle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bundle = AssetBundle.LoadFromFile("Assets/Scenes");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadMap()
+    {
+        SceneManager.LoadScene("TestBattle");
+    }
+
+    public void loadCombat()
+    {
+        SceneManager.LoadScene("TestingCharacter");
+    }
+}
+
